e514f0b797b11a05cadde57cdd75cefd5a92bf28
Michael Heu <mheu@clickbooq.com>
2012-07-12 15:35:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '341ca7ed9fa2c0d467be051aeb4caa45cf871042404d38966af998c74bff4008af460ff62c53b3231a2f57c920c9e0e503e869466f8b5fdc5efa4d3423ac12f2'
